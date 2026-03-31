4cfc9cc7fcf3be5d4e465d1be00ae86be342af06
FireBlaze245 <dannosorev@gmail.com>
2026-03-31 23:25:02 +0300

    ghg

---
+++ main.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+import tracemalloc
+from os import getenv
+from aiogram import Bot, Dispatcher, Router
+from dotenv import load_dotenv
+from aiogram.client.session.aiohttp import AiohttpSession
+
+session = AiohttpSession(proxy="http://95.213.217.168:52004")
+load_dotenv()
+TOKEN = getenv('BOT_TOKEN')
+if not TOKEN:
+    raise ValueError("Token not found!")
+print(TOKEN)
+
+dp = Dispatcher()
+router = Router()
+dp.include_router(router)
+
+@router.message()
+async def hello(message):
+    await message.answer("Hello!")
+
+async def main():
+    bot = Bot(token=TOKEN, session=session, request_timeout=30)
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    tracemalloc.start()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Exit')
