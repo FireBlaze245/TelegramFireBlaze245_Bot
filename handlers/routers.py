0c7b68a403c73089b87949820ec3b1cbc08ecf95
FireBlaze245 <dannosorev@gmail.com>
2026-04-01 17:30:26 +0300

    the second fully working version of running a bot with a proxy has been recorded

--- handlers/routers.py
+++ handlers/routers.py
@@ -1,6 +1,7 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command
-from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton
+from aiogram.types import (Message, ReplyKeyboardMarkup, KeyboardButton,
+                           InlineKeyboardButton, InlineKeyboardMarkup, callback_query, CallbackQuery)
 
 router = Router()
 
@@ -9,20 +10,38 @@ def get_main_reply_keyboard():
     keyboard = ReplyKeyboardMarkup(keyboard=[
         [KeyboardButton(text="О нас")],
         [KeyboardButton(text="Старт"), KeyboardButton(text="Помощь")]
-    ])
+    ],resize_keyboard=True)
+    return keyboard
 
 
+def get_main_inline_keyboard():
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [InlineKeyboardButton(text = "Открыть сайт", url="http://google.com")],
+            [InlineKeyboardButton(text = "/start", callback_data="start")],
+        ]
+    )
+    return keyboard
+
+
+@router.callback_query(lambda c: c.data == "start")
+async def callback_start(callback: CallbackQuery):
+    await callback.message.answer("Хахахахахахах")
+    await callback.answer()
+
+@router.message(F.text.lower() == "старт")
 @router.message(Command("start"))
 async def start(message: Message):
     await message.answer("You're welcome!\n\nНапиши /help для помощи")
 
 @router.message(Command("help"))
 async def help(message: Message):
-    await message.answer("Команды:\n/start - запуск бота\n/help - комманды\n/about - о нас")
+    await message.answer("Команды:\n/start - запуск бота\n/help - комманды\n/about - о нас",
+                         reply_markup=get_main_reply_keyboard())
     
 @router.message(Command("about"))
 async def about(message: Message):
-    await message.answer(f"Этот бот мой, {message.from_user.first_name}")
+    await message.answer(f"Этот бот мой, {message.from_user.first_name}", reply_markup=get_main_inline_keyboard())
 
 @router.message()
 async def hello(message):
